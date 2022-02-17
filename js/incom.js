cdbe874de4b02d93c43b6953e73b40a9d7147dc8
MohammadFaysal731 <www.faysal731@gmail.com>
2022-02-17 15:03:59 +0600

    set the total expenses & balance with out erro handel

--- js/incom.js
+++ js/incom.js
@@ -1,1 +1,26 @@
-console.log('heelo')
+
+// All expenses input values
+function getInputValues(inputs) {
+    const allInputs = document.getElementById(inputs + '-input');
+    const allNumber = parseInt(allInputs.value);
+    // clear inputs fields
+    allInputs.value = '';
+    return allNumber;
+}
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    //    Income
+    const incomeNumber = getInputValues('income');
+    // Expenses 
+    const footNumber = getInputValues('foot');
+    const rentNumber = getInputValues('rent');
+    const clothesNumber = getInputValues('clothes');
+    const tottalExpenses = document.getElementById('tottal-expenses');
+    const totalExpenses = footNumber + rentNumber + clothesNumber;
+    tottalExpenses.innerText = totalExpenses;
+    // calculate balance 
+    const balance = document.getElementById('balance');
+    const newBalance = incomeNumber - totalExpenses;
+    balance.innerText = newBalance;
+
+
+});
