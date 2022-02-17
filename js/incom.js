4ca5237b689b2ff8950cd868789cd608b744951a
MohammadFaysal731 <www.faysal731@gmail.com>
2022-02-17 22:26:57 +0600

    shoew error

--- js/incom.js
+++ js/incom.js
@@ -3,46 +3,54 @@
 function getInputValues(inputs) {
     const allInputs = document.getElementById(inputs + '-input');
     const allNumber = parseFloat(allInputs.value);
-    // clear inputs fields
-    allInputs.value = '';
-    if (isNaN(allNumber) || allNumber > 0) {
-        return alert("Pleas full fill the all input field, and don't give string or nagative numbers.only give positive number")
+    if (isNaN(allNumber) || allNumber < 0) {
+        return alert("Please refresh this pages, full fill the all input field, and don't give string or nagative numbers.only give positive number")
     }
     else {
         return allNumber;
     }
-
-}
-
+};
 
 // calculate handel by eventlistener 
 document.getElementById('calculate-btn').addEventListener('click', function () {
+
     //    Income
     const incomeAmount = getInputValues('income');
     // Expenses 
     const footNumber = getInputValues('foot');
     const rentNumber = getInputValues('rent');
     const clothesNumber = getInputValues('clothes');
-    if (totalExpenses > 0 || newBalance > 0) {
-        // Total expenses 
-        const tottalExpenses = document.getElementById('tottal-expenses');
-        const totalExpenses = footNumber + rentNumber + clothesNumber;
+    // Total expenses 
+    const tottalExpenses = document.getElementById('tottal-expenses');
+    const totalExpenses = footNumber + rentNumber + clothesNumber;
+
+    // calculate balance 
+    if (incomeAmount < totalExpenses) {
+        return alert('Your income is small your expenses');
+    }
+
+    else {
         tottalExpenses.innerText = totalExpenses;
-        // calculate balance 
         const balance = document.getElementById('balance');
         const newBalance = incomeAmount - totalExpenses;
         balance.innerText = newBalance;
     }
 });
+function savingInput() {
+    const saveInput = getInputValues('save');
+    const incomeInput = getInputValues('income');
+    const savingAmount = incomeInput / saveInput;
+    const savedAmount = savingAmount;
 
-// /* document.getElementById('income-save').addEventListener('click', function () {
-//     /*  const saveIncome = document.getElementById('save-input');
-//      const saveNumber = parseFloat(saveIncome.value);
-//      saveIncome.value = saveNumber; */
-// const saveIncome = getInputValues('save');
-// const newBalance = document.getElementById('balance');
-// const savedAmount = saveIncome / newBalance;
-// newBalance.value = savedAmount;
-// const savingAmount = document.getElementById('saving-amount');
-// savingAmount.innerText =
-// }); * /   
+    if (isNaN(savingAmount) || savingAmount < 0) {
+        return alert("Please refresh this pages, full fill the all input field, and don't give string or nagative numbers.only give positive number")
+    }
+    else {
+        return savedAmount;
+    }
+};
+// saving amount of my income 
+document.getElementById('income-save').addEventListener('click', function () {
+    const savedAmount = document.getElementById('saving-amount');
+    savedAmount.innerText = savingInput();
+}); 
